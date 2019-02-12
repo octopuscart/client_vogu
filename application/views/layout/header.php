21fa80b6da0fbe21945452202d365c6d6092c75a
dev1 <dev@octopuscart.com>
2019-02-12 15:19:00 +0530

    cahngews

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -21,6 +21,8 @@
 
         <!-- CSS -->
         <link rel="stylesheet" href="<?php echo base_url(); ?>assets/theme/css/bootstrap.min.css">
+        <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
         <link rel="stylesheet" href="<?php echo base_url(); ?>assets/theme/css/style.css">
         <link rel="stylesheet" href="<?php echo base_url(); ?>assets/theme/css/style-responsive.css">
         <link rel="stylesheet" href="<?php echo base_url(); ?>assets/theme/css/animate.min.css">
